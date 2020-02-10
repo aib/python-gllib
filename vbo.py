0f3daf1e460d245dca8cb5903822dd5f720b1545
aib <aibok42@gmail.com>
2020-02-10 16:29:32 +0300

    Add VBO class

--- vbo.py
+++ vbo.py
@@ -1,1 +1,10 @@
 from .gl import GL
+
+class VBO:
+	def __init__(self, buffer_type, usage_hint):
+		self.id = GL.glGenBuffers(1)
+		self.type = buffer_type
+		self.hint = usage_hint
+
+	def activate(self):
+		GL.glBindBuffer(self.type, self.id)

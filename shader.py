963b8dd42bbe2162b9a2da1ebee20c60b8624ab5
aib <aibok42@gmail.com>
2020-02-10 15:40:23 +0300

    Add VertexFragmentProgram class

--- shader.py
+++ shader.py
@@ -50,3 +50,10 @@ class Program:
 
 	def activate(self):
 		GL.glUseProgram(self.id)
+
+class VertexFragmentProgram(Program):
+	def __init__(self, vertex_shader, fragment_shader):
+		super().__init__()
+		self.add_shader(GL.GL_VERTEX_SHADER, vertex_shader)
+		self.add_shader(GL.GL_FRAGMENT_SHADER, fragment_shader)
+		self.link()

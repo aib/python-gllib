cbe86749c9f7111332bc771da65ef2479ae2b724
aib <aibok42@gmail.com>
2020-02-10 12:50:36 +0300

    Add get_uniform_location (and UniformNotFound)

--- uniform.py
+++ uniform.py
@@ -1,1 +1,12 @@
 from .gl import GL
+
+class UniformNotFound(Exception):
+	def __init__(self, uniform_name):
+		super().__init__("Uniform \"%s\" not found" % (uniform_name,))
+
+def get_uniform_location(program_id, name, silent=False):
+	location = GL.glGetUniformLocation(program_id, name)
+	if location == -1:
+		if not silent:
+			raise UniformNotFound(name)
+	return location

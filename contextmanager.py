990c4884fc8fedfc6d3a0b9306c6b035ae05a413
aib <aibok42@gmail.com>
2020-02-11 12:02:06 +0300

    Add context managers (__enter__ and __exit__) to activatable classes

--- contextmanager.py
+++ contextmanager.py
@@ -1,5 +1,14 @@
 def activatable(cls):
+	cls.__enter__ = _activatable_enter
+	cls.__exit__ = _activatable_exit
 	return cls
 
 def activated(func):
 	return func
+
+def _activatable_enter(self):
+	self.activate()
+	return self
+
+def _activatable_exit(self, exc_type, exc_value, traceback):
+	pass

ac5c6a794efb02249b685da1afc5668def659c59
aib <aibok42@gmail.com>
2020-02-12 16:56:50 +0300

    Add Texture base class

--- texture.py
+++ texture.py
@@ -1,1 +1,19 @@
 from .gl import GL
+from . import contextmanager
+
+@contextmanager.activatable
+class Texture:
+	def __init__(self, texture_type, binding_type):
+		self.type = texture_type
+		self.binding_type = binding_type
+
+		self.id = GL.glGenTextures(1)
+		self.last_active_id = None
+
+	def activate(self):
+		self.last_active_id = GL.glGetInteger(self.binding_type)
+		GL.glBindTexture(self.type, self.id)
+
+	def deactivate(self):
+		GL.glBindTexture(self.type, self.last_active_id)
+		self.last_active_id = None

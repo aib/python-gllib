a06c1672ee8ca2bbf406e25e730956e96c159b88
aib <aibok42@gmail.com>
2020-02-13 03:59:40 +0300

    Add contextmanager.activator decorator for non-default activator functions

--- contextmanager.py
+++ contextmanager.py
@@ -1,3 +1,5 @@
+import functools
+
 def activatable(cls):
 	cls.__enter__ = _activatable_enter
 	cls.__exit__ = _activatable_exit
@@ -5,6 +7,24 @@ def activatable(cls):
 
 def activated(func):
 	return func
+
+def activator(func):
+	@functools.wraps(func)
+	def wrapper(self, *args, **kwargs):
+		func(self, *args, **kwargs)
+		return _ActivatorProxy(self)
+
+	return wrapper
+
+class _ActivatorProxy:
+	def __init__(self, original):
+		self.original = original
+
+	def __enter__(self):
+		return self.original
+
+	def __exit__(self, exc_type, exc_value, traceback):
+		pass
 
 def _activatable_enter(self):
 	self.activate()

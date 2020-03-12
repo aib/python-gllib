6f369ebabdd851f6db53a5ec721cb4eb45402210
aib <aibok42@gmail.com>
2020-03-12 12:22:19 +0300

    Use actual helpers activate and deactivate, call them in activator activation as well

--- contextmanager.py
+++ contextmanager.py
@@ -3,11 +3,11 @@ import functools
 def activatable(cls):
 	def activatable_enter(self):
 		self.activate()
+		_activate(self)
 		return self
 
 	def activatable_exit(self, exc_type, exc_value, traceback):
-		if hasattr(self, 'deactivate'):
-		self.deactivate()
+		_deactivate(self)
 
 	cls.__enter__ = activatable_enter
 	cls.__exit__ = activatable_exit
@@ -29,7 +29,15 @@ class _ActivatorProxy:
 		self.original = original
 
 	def __enter__(self):
+		_activate(self)
 		return self.original
 
 	def __exit__(self, exc_type, exc_value, traceback):
-		pass
+		_deactivate(self.original)
+
+def _activate(self):
+	pass
+
+def _deactivate(self):
+	if hasattr(self, 'deactivate'):
+		self.deactivate()
